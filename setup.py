5b44114497705550431082bf42128da04f4425cf
shicheng <alistar.huang@gmail.com>
2024-12-31 10:32:56 -0800

    add pypi publication

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 # install requirements from requirements.txt
+# add github link to setup.py
 setup(
     name="askharrison",
-    version="0.1",
+    version="0.1.2c",
     packages=find_packages(),
+    description="A package for the AskHarrison, including various GenAI tools",
+    long_description="A package for the AskHarrison, including various GenAI tools",
+    url="https://github.com/shex1627/askharrison",
+    project_urls={                                    # Additional links
+        "Source Code": "https://github.com/shex1627/askharrison",
+    },
     install_requires=[
         "streamlit",
         "requests",
